171b639550358b51b9a4ae38348827fcf90a36d4
dk17 <darya.kononovich17@gmail.com>
2025-05-15 00:26:56 +0200

    working new ui

--- src/app/services/shopping.service.ts
+++ src/app/services/shopping.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface ShoppingItem {
+  _id?: string;
+  name: string;
+  bought: boolean;
+  store?: string;
+  date?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingService {
+  private apiUrl = 'http://127.0.0.1:5000/items';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getItems(): Observable<ShoppingItem[]> {
+    return this.http.get<ShoppingItem[]>(this.apiUrl);
+  }
+
+  addItem(item: ShoppingItem): Observable<any> {
+    return this.http.post(this.apiUrl, item);
+  }
+
+  updateItem(id: string, item: ShoppingItem): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, item);
+  }
+
+  deleteItem(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
--- src/app/shopping-list/list.component.ts
+++ src/app/shopping-list/list.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingService} from "../services/shopping.service";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+
+interface ShoppingItem {
+  _id?: string;
+  name: string;
+  bought: boolean;
+  store?: string;
+  date?: string;
+}
 
 @Component({
   selector: 'app-list',
-  standalone: true,
-  imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
+  ],
+  standalone: true,
+  styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  items: ShoppingItem[] = [];
+  newItemForm: FormGroup;
 
+  constructor(
+    private shoppingService: ShoppingService,
+    private fb: FormBuilder
+  ) {
+    this.newItemForm = this.fb.group({
+      name: [''],
+      store: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadItems();
+  }
+
+  loadItems() {
+    this.shoppingService.getItems().subscribe((data: any) => {
+      this.items = data;
+    });
+  }
+
+  addItem() {
+    const item: ShoppingItem = {
+      name: this.newItemForm.value.name,
+      store: this.newItemForm.value.store,
+      bought: false,
+      date: new Date().toISOString()
+    };
+    this.shoppingService.addItem(item).subscribe(() => {
+      this.newItemForm.reset();
+      this.loadItems();
+    });
+  }
+
+  updateItem(item: ShoppingItem) {
+    if (!item._id) return;
+    this.shoppingService.updateItem(item._id, item).subscribe(() => this.loadItems());
+  }
+
+  deleteItem(id: string) {
+    this.shoppingService.deleteItem(id).subscribe(() => this.loadItems());
+  }
+
+  toggleBought(item: ShoppingItem) {
+    item.bought = !item.bought;
+    this.updateItem(item);
+  }
 }
